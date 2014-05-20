cfc9adf661852ac2dca279361e19e32506ab4922
Lauren Morita <lmorita@dhcp-10-10-209-174.net.pacific.edu>
2014-05-20 14:29:06 -0700

    Initial Commit

---
+++ ScribbleScript/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScribbleScript
+//
+//  Created by Lauren Morita on 5/20/14.
+//  Copyright (c) 2014 CosmicLatté. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScribbleScript/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScribbleScript
+//
+//  Created by Lauren Morita on 5/20/14.
+//  Copyright (c) 2014 CosmicLatté. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
